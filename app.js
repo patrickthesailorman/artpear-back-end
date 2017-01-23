453e30bc95eea0d181e09678dc68d36f69362a80
Jesse Ellis <jessetellis@gmail.com>
2017-01-23 18:09:58 -0500

    Create POC get and show routes for Artist

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+var artistsRouter = require('./app/routers/artists');
 var port = process.env.PORT || 8080;
+
+app.use('/artists', artistsRouter);
 
 app.listen(port, () => {
   console.log(`Listening on :${port}`);
---
+++ app/routers/artists.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var Artist = require('../models/artist');
+
+router.get('/', (req, res) => {
+  Artist.find((err, artists) => {
+    if(err) console.error(err);
+    res.send(artists);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  Artist.findById(req.params.id, (err, artist) => {
+    if(err) console.error(err);
+    res.send(artist);
+  });
+});
+
+module.exports = router;
