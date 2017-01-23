c481d32fa1093a9f85d4a76e27f9936728fc61db
Jesse Ellis <jessetellis@gmail.com>
2017-01-23 16:33:44 -0500

    Seed database with example Artist and Piece

---
+++ db/seed.js
@@ -0,0 +1,39 @@
+var mongoose  = require('./connection');
+
+var User = require('../app/models/user');
+var Artist = require('../app/models/artist');
+var Piece = require('../app/models/piece');
+
+var artistSeed = {
+  firstName: 'Hector',
+  lastName: 'Suave',
+  organizationName: "Hector's Creations",
+  email: 'hector.s@example.com',
+  bio: "I'm a local aritst with a lot of great ideas.",
+  photoUrl: 'http://i.imgur.com/dqycl3D.png'
+};
+
+var pieceSeed = {
+  title: 'War and Peace',
+  description: "It's existential and relevant: a tour de force.",
+  photoUrl: 'http://i.imgur.com/Nhm7kTX.jpg'
+};
+
+User.remove({kind: 'Artist'}).then(() => {
+  Piece.remove({}).then(() => {
+    var artist = new Artist(artistSeed);
+    artist.save((err) => {
+      if(err) console.error(err);
+
+      pieceSeed._artist = artist._id;
+      var piece = new Piece(pieceSeed);
+
+      piece.save((err) => {
+        if(err) console.error(err);
+
+        console.log('\nArtist:\n', artist, '\nPiece:\n', piece);
+        process.exit();
+      });
+    });
+  });
+});
