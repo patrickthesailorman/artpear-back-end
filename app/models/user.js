72bb929cf3cab430208d55b8926829da8a139bab
Jesse Ellis <jessetellis@gmail.com>
2017-01-23 11:48:01 -0500

    Define additional fields for User

--- app/models/user.js
+++ app/models/user.js
@@ -2,14 +2,15 @@ var mongoose = require('../../db/connection');
 var Schema = mongoose.Schema;
 
 var User = new Schema({
-  firstName: String,
-  lastName: String,
-  email: { //functions as unique username
+  firstName: String, //required
+  lastName: String, //required
+  organizationName: String, //optional
+  email: { //required, unique
     type: String,
     unique: true
   },
-  bio: String,
-  photoUrl: String
+  bio: String, //optional
+  photoUrl: String //optional
 });
 
 module.exports = mongoose.model('User', User);
