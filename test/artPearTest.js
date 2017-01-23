4d3b3115a1220b2aaef490f04e57035a50483686
“neilburd” <neilburd@gmail.com>
2017-01-23 15:59:10 -0500

    Added mocha and chai, thought its a good idea to start unit testing this project.

---
+++ test/artPearTest.js
@@ -0,0 +1,13 @@
+// https://semaphoreci.com/community/tutorials/getting-started-with-node-js-and-mocha
+var expect  = require("chai").expect;
+var artPear = require(' '); //// include the file we want to test
+describe("Art Pear Testing", function(){
+  describe("Model", function(){
+    it("Populates with all fields", function(){
+      //// add what we need to expect here
+      var model = //////
+      expect(model).to.have.lengthOf(#);///// add number we want expect it should have.
+
+    });
+  });
+});
