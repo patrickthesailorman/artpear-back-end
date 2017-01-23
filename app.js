98b77c7dbd708ef37884819766fdc35cea33671d
Jesse Ellis <jessetellis@gmail.com>
2017-01-22 21:13:17 -0500

    Configure Passport

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
 var app = express();
 var artistsRouter = require('./app/routers/artists');
 var seekersRouter = require('./app/routers/seekers');
@@ -7,6 +9,20 @@ var opportunitiesRouter = require('./app/routers/opportunities');
 var responsesRouter = require('./app/routers/responses');
 
 var port = process.env.PORT || 8080;
+var User = require('./app/models/user');
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// https://github.com/expressjs/session#sessionoptions
+app.use(session({
+  secret: '?s2}FX84e?4gC43nCyB7',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
