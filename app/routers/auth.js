1238e81c205970fac85140a109130125a2b2e5fb
Jesse Ellis <jessetellis@gmail.com>
2017-01-28 20:41:28 -0500

    Session created at login and destroyed at logout

--- app/routers/auth.js
+++ app/routers/auth.js
@@ -3,11 +3,11 @@ var passport = require('passport');
 var User = require('../models/user');
 var router = express.Router();
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login', passport.authenticate('local'), (req, res, next) => {
   res.sendStatus(200);
 });
 
-router.post('/logout', (req, res) => {
+router.post('/logout', (req, res, next) => {
   req.logout();
   res.sendStatus(200);
 });
